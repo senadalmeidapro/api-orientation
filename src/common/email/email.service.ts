5de9b34c4f9df68debbe01d94de0ab517a840f40
senadalmeidapro <senadalmeidapro@gmail.com>
2026-04-20 11:48:52 +0100

    fix: resolve email service connection timeout during template compilation

--- src/common/email/email.service.ts
+++ src/common/email/email.service.ts
@@ -88,6 +88,7 @@ export class EmailService implements OnModuleInit {
 
         // Init client Brevo (utilisé en production)
         const client = SibApiV3Sdk.ApiClient.instance;
+        console.log("DEBUG EMAIL API KEY: ", this.config.email.brevo.apiKey);
         client.authentications['api-key'].apiKey = this.config.email.brevo.apiKey;
         this.brevoClient = new SibApiV3Sdk.TransactionalEmailsApi();
 
